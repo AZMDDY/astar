49ea623e042181e29a3de17bf79ec872d104fe61
zzb <azmddy@163.com>
2020-11-01 07:39:20 +0000

    check overflow

--- include/point.hpp
+++ include/point.hpp
@@ -1,5 +1,6 @@
 #ifndef ASTAR_POINT_HPP_
 #define ASTAR_POINT_HPP_
+#include "base.hpp"
 
 namespace astar {
 class Point {
@@ -24,6 +25,11 @@ inline bool Point::operator==(const Point& p) const
 
 Point& Point::operator+=(const Point& p)
 {
+    if ((p.x > 0 && INT32_MAX - p.x < x) || (p.y > 0 && INT32_MAX - p.y < y) || (p.x < 0 && INT32_MIN - p.x > x) ||
+        (p.y < 0 && INT32_MIN - p.y > y)) {
+        ALOG() << "add point is out of range.";
+        return *this;
+    }
     x += p.x;
     y += p.y;
     return *this;
@@ -31,6 +37,11 @@ Point& Point::operator+=(const Point& p)
 
 Point operator+(const Point& aPoint, const Point& bPoint)
 {
+    if ((aPoint.x > 0 && INT32_MAX - aPoint.x < bPoint.x) || (aPoint.y > 0 && INT32_MAX - aPoint.y < bPoint.y) ||
+        (aPoint.x < 0 && INT32_MIN - aPoint.x > bPoint.x) || (aPoint.y < 0 && INT32_MIN - aPoint.y > bPoint.y)) {
+        ALOG() << "add point is out of range.";
+        return {0, 0};
+    }
     return {aPoint.x + bPoint.x, aPoint.y + bPoint.y};
 }
 
